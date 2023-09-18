cea450c881b5e17070349d45da4e8c5c1ef4c2e5
AbdelaleemAhmed <ahmedadel2912@gmail.com>
2023-09-18 18:56:45 +0300

    Create the profile menu(login/signup/logout)

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 declare var $:any;
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  accountMenu:any;
 constructor(){
 }
-
+ngOnInit(): void {
+  this.accountMenu = $('.sign');
+}
 /* Close the navBar in the small screens */
 closeNav():void{
 if($('.nav-search').css('display')
 =='none')
 {  $('.above-all').hide();
 }
-$('.toggled-nav').animate({left:'-100%'},600)
+$('.toggled-nav').animate({left:'-300px'},600)
 }
 /* Open the nav bar in small screen */
 openNav():void{
@@ -37,5 +41,14 @@ closeNavSearch():void{
 
   $('.nav-search').fadeOut();
 }
+toggleAccountMenu():void{
+ this.accountMenu.fadeToggle()
+}
 }
 
+ $('body, html').click(function(event:any){
+if(event.target != $('.fa-user')[0] && event.target != $('.fa-user')[1]){
+  $('.sign').css("display", "none")
+  console.log('closed');
+}
+})
